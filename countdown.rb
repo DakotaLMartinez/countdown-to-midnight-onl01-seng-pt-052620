2eecd6b0da8796151f2e33630f13d383a1acdd13
Dakota Martinez <dakotaleemusic@gmail.com>
2020-06-01 05:48:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(seconds_left)
+  until seconds_left < 1
+    puts "#{seconds_left} SECOND(S)!"
+    seconds_left -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
+end
+
+# number -= 1 #=> number = number - 1
+
+def countdown_with_sleep(seconds_left)
+  until seconds_left < 1
+    puts "#{seconds_left} SECOND(S)!"
+    seconds_left -= 1
+    sleep 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
